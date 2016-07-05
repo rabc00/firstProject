6b3599c40df066783f556f420f758f032d139b62
rabc00 <RBOLAN2207@suscorp.edu>
2016-07-05 19:02:19 -0400

    My first check
    Added all the files

---
+++ firstProject/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Playing With C#";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Blog() 
+        {
+            ViewBag.Message = "Your Blog Page";
+
+            return View();
+        }
+    }
+}
